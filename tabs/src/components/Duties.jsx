30707108b946c5dd4224e9d922824c761dac6d05
MLDODSON <paola@dominicansoul.com>
2024-01-25 11:22:43 -0600

    Branch 5
    Branch 5 - Updated Duties.jsx.

--- tabs/src/components/Duties.jsx
+++ tabs/src/components/Duties.jsx
@@ -1,9 +1,26 @@
+import { FaAngleDoubleRight } from "react-icons/fa";
+import {v4 as uuidv4} from "uuid";
+
 const Duties = ({ duties }) => {
-    console.log(duties); 
-    
+
     return (
-      <h1>Duties</h1>
-    )
+      <div>
+        {duties.map((duty, index) => {
+          const id = uuidv4();
+          // console.log(id);
+
+          return (
+            <div 
+              key={id}
+              className="grid grid-cols-[auto,1fr] gap-8 items-center mb-5"
+            >
+              <FaAngleDoubleRight className="text-[#4b0596]" />
+              <p className="text-[#334155] leading-6">{duty}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
   };
   
   export default Duties;
